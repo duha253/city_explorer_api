cbad0d254ffbd594ecbeaa8a094e67d5348d5eae
duha253 <duhayousef26@gmail.com>
2021-03-31 16:12:12 +0300

    do

--- schema.sql
+++ schema.sql
@@ -5,5 +5,5 @@ CREATE TABLE location
 (id SERIAL PRIMARY KEY NOT NULL,
 search_query  VARCHAR(255),
 formatted_query VARCHAR(255),
-latitude VARCHAR(255),
-longitude VARCHAR(255));
+latitude NUMERIC(10,7),
+longitude NUMERIC(10,7));
